9a2c57be39bda95e32241bfe6ef041d476805324
chazo1994 <chazo1994@gmail.com>
2017-08-22 00:33:14 +0700

    first create project and add average filter

---
+++ ImageFilter/AverageFilter.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import numpy as np
+def LoadImage(url, filterSize):
+    #with numpy array [i,j,k] k: r,g,b,s; i,j: axist of image
+    mImage = np.asarray(Image.open(url))
+    applyFilter(mImage, filterSize)
+
+def applyFilter(mImage, filterSize):
+    #print(mImage[100,105,2])
+    red=mImage[:,:,0]
+    green=mImage[:,:,1]
+    blue=mImage[:,:,2]
+    print(red.shape)
+    print(np.size(red[:,1]))
+    print(np.size(red[1,:]))
+    im = Image.fromarray(red)
+    im = Image.fromarray(green)
+    im = Image.fromarray(blue)
+    print("done")
+
+def filter(mColor, filtersize):
+    print("color")
+    xlength = np.size(mColor[:,1])
+    ylength = np.size(mColor[1,:])
+    for i in range(0, xlength):
+        for j in range(0, ylength):
+            print("somthing")
---
+++ ImageFilter/main.py
@@ -0,0 +1,2 @@
+import AverageFilter as av
+av.LoadImage('/home/thinh/Desktop/MyProject/ImageFilter/result.png',3)
